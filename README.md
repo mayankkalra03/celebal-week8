490b145be76cee3c54aa728601b1f256c6eeaa49
Mayank Kalra <mayankkalravp@gmail.com>
2024-07-14 02:01:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# E-Commerce Website
+
+A Ecommerce Website made with React.js Framework.
+
+## Features
+
+- Easy to integrate with Backend
+- Fully Responsive
+
+## Getting Started
+
+### 1. Clone the repository
+
+Clone the repository to your local machine using the following command:
+
+```sh
+git clone https://github.com/mayankkalra03/celebal-week8.git
+cd celebal-week8
+```
+
+### 2. Install dependencies
+
+Install the necessary dependencies by running:
+
+```sh
+npm install
+```
+
+### 3. Start the application
+
+Launch the application with:
+
+```sh
+npm start
+```
+
+### 4. Visit the application
+
+Open http://localhost:3000 to view the application in your web browser.
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        Welcome to Mayank's Store, where fashion meets quality and affordability. Founded in 2024, our mission is to bring you the latest trends and timeless classics that fit your style and budget. We believe that everyone deserves to look and feel their best, and our curated collections are designed to do just that.<br></br>
+        <br />
+        At Mayank's Store, we pride ourselves on offering a diverse range of apparel for men, women, and children. From casual wear to formal attire, our selection is tailored to meet the needs of every occasion and lifestyle. Our team of fashion experts carefully selects each piece to ensure it meets our high standards of quality, comfort, and style.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
